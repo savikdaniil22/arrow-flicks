8da1fcda554d90918697d98dc8ab2aec725f781a
Savik Daniil <savikdanii3@gmail.com>
2024-05-22 22:47:00 +0300

    Add 404 page and rated page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+import styles from './not-found.module.scss';
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <div className={styles.notFound}>
+        <div className={styles.notFounddItem}>
+          <Image src="/notfound.svg" alt="notFound" width={656} height={196} />
+          <p>We can’t find the page you are looking for</p>
+          <a href="/movies">Find movies</a>
+        </div>
+      </div>
+    </div>
+  );
 }
